4be4fc78c12bd6874f598ecc8f54f1bb25ad87f3
raihanryan9 <raihanryan99@gmail.com>
2023-02-05 14:10:23 +0700

    menambahkan tugas uas

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>UTSRIAN</title>
+    <title>UAS RIAN RAIHAN M</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="./tailwind.config.js"></script>
     <style type="text/tailwindcss">
@@ -21,7 +21,7 @@
 <body class="bg-[#100E0B] text-slate-100">
     <header class="bg-header">
         <section class="container mx-auto">
-            <section id="topbar" class="flex justify-between gap-10 items-center py-5 px-5" style="border: 2px solid salmon;">
+            <section id="topbar" class="flex justify-between gap-10 items-center py-5 px-5">
                 <div id="left-side" class="flex gap-10 items-center">
                     <div class="flex gap-3">
                         <img src="./property/ic_round-table-restaurant.png" alt="">
@@ -59,8 +59,8 @@
         </section>
         <section>
             <div class="container">
-                <div class="main-hamburger flex justify-center gap-3" style="border: 2px solid salmon;">
-                    <div class="w-[30%]" style="border: 2px solid salmon;">
+                <div class="main-hamburger flex justify-center gap-3">
+                    <div class="w-[30%]">
                         <h3 class="text-[#FF9900] py-7">Best Product</h3>
                         <h1 class="text-3xl py-4"> <span class="font-bold">Hamburger</span> Best in The Universe</h1>
                         <p class="text-sm py-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque iusto corrupti molestias ad, ipsum totam?</p>
@@ -69,10 +69,10 @@
                             <li><a href="#" class="py-1 px-7 border-2 border-slate-400 rounded-xl">View Menu →</a></li>
                         </ul>
                     </div>
-                    <div class="w-[30%]" style="border: 2px solid salmon;">
+                    <div class="w-[30%]">
                         <img src="property/image 5.png" alt="">
                     </div>
-                    <div class="w-[30%]" style="border: 2px solid salmon;">
+                    <div class="w-[30%]">
                         <p class="text-center">100</p>
                     </div>
                 </div>
@@ -98,8 +98,8 @@
     <!-- end -->
     <section>
         <div class="container">
-            <div class="main-hamburger flex justify-center gap-3" style="border: 2px solid salmon;">
-                <div class="w-[60%]" style="border: 2px solid salmon;">
+            <div class="main-hamburger flex justify-center gap-3 my-20">
+                <div class="w-[40%]">
                     <h3 class="text-[#FF9900] py-7">About Us</h3>
                     <h1 class="text-3xl py-4 font-bold">MaemMamam</h1>
                     <p class="text-sm py-3">We were founded in 1948 after the war and the world began to be safe we decided to open a small restaurant on the side of the road for the youth and the surrounding community, until finally we had the capital to create the only largest restaurant and so far we have opened 10 branches in Indonesia.</p>
@@ -107,7 +107,7 @@
                         <li><a href="#" class="py-1 px-7 border-2 border-slate-400 rounded-xl">View Menu →</a></li>
                     </ul>
                 </div>
-                <div class="w-[40%] flex justify-center" style="border: 2px solid salmon;">
+                <div class="w-[40%] flex justify-center">
                     <img src="property/Group 2076.png" class="w-[392px]" alt="">
                 </div>
             </div>
@@ -116,11 +116,11 @@
     <!-- end -->
     <section>
         <div class="container">
-            <div class="main-hamburger flex justify-center gap-3" style="border: 2px solid salmon;">
-                <div class="w-[40%] flex justify-center" style="border: 2px solid salmon;">
+            <div class="main-hamburger flex justify-center gap-3 my-20">
+                <div class="w-[40%] flex justify-center">
                     <img src="property/Group 2107.png" class="w-[392px]" alt="">
                 </div>
-                <div class="w-[60%]" style="border: 2px solid salmon;">
+                <div class="w-[60%]">
                     <h3 class="text-[#FF9900] py-7">MaemMamam Restaurant</h3>
                     <h1 class="text-3xl py-4"> <span class="font-bold">Fast food and fill your stomach</h1>
                     <p class="text-sm py-3">we are a fast food company and we make everyone enjoy any food we serve them because we serve the best for them all.</p>
@@ -136,16 +136,16 @@
         <div class="container">
             <div class="main-menu">
                 <div class="header-menu w-[80%] mx-auto">
-                    <h3 class="text-[#FF9900] py-7">Popular menu</h3>
-                    <h1 class="text-3xl py-4 font-bold">Best Popular Menu</h1>
+                    <h3 class="text-[#FF9900] py-5">Popular menu</h3>
+                    <h1 class="text-3xl py-4 font-bold ">Best Popular Menu</h1>
                 </div>
             </div>
-            <div class="more flex w-[80%] mx-auto">
+            <div class="more flex w-[80%] justify-between mx-auto">
                 <div class="w-1/2">
-                    <p>Find the food you want to buy and get the prize, made from the best ingredients we choose</p>
+                    <p class="my-5">Find the food you want to buy and get the prize, made from the best ingredients we choose</p>
                 </div>
-                <div class="w-1/2 flex justify-end">
-                    <a href="#">View More</a>
+                <div class=" p-5 h-11 items-center flex justify-end border-2 border-slate-400 rounded-xl">
+                    <a href="#" class="">View More</a>
                 </div>
             </div>
             <div class="menu flex justify-center items-center gap-3 flex-wrap">
@@ -310,7 +310,54 @@
                 </div>
             </div>
         </div>
+        <div class="px24 py-24 w-[80%] mx-auto justify-center items-start rounded-xl my-32"  style="border: 2px solid white;">
+            <div class="text-center ">
+            <h3 class="text-[#FF9900] text-sm my-5">Ordere now</h3>
+            <h1 class="font-bold text-4xl my-5">Order Now And Take a Giv away</h1>
+            <h2>Order and get the prize, we will give a gift to those of you who are lucky to get something interesting from us</h2>
+            <ul><li class="my-10"><a href="#" class="py-1 px-7 bg-[#FF9900] rounded-xl">Order Now</a></li></ul>
+            </div>
+        </div>
     </section>
-    <!-- end -->
-    <div class="mb-96"></div>
+    <footer class="w-[80%] mx-auto bg-[#100E0B]" >
+        <div class="px24 py-24 max-w-screen-xl mx-auto flex justify-start items-start gap-7">
+            <div class="w-[30%]">
+                <div class="flex gap-0">
+                    <img src="./property/ic_round-table-restaurant.png" alt="">
+                    <span>
+                        <h1 class="flex justify-center items-center mt-2">maemamam</h1>
+                    </span>
+                </div>
+                <p class="text-sm py-20">We were founded in 1948 after the war and the world began to be safe we decided to open a small restaurant on the side of the road for the youth and the surrounding community.</p>
+                <ul class="flex justify-start gap-10 my-10">
+                    <li><a href="#"><img src="./property/facebook logo.png" alt=""></a></li>
+                    <li><a href="#"><img src="./property/Twitter logo.png" alt=""></a></li>
+                    <li><a href="#"><img src="./property/linkedin logo.png" alt=""></a></li>
+                    <li><a href="#"><img src="./property/instagram logo.png" alt=""></a></li>
+                </ul>
+            </div>
+            <div class="w-[20%] mt-28">
+                <h1 class="text-2xl font-bold mb-5">Navigation</h1>
+                <ul>
+                    <li class="mb-5"><a href="#">home</a></li>
+                    <li class="mb-5"><a href="#">info</a></li>
+                    <li class="mb-5"><a href="">news</a></li>
+                    <li class="mb-5"><a href=""> About Us</a></li>
+                    <li class="mb-5"><a href="">Contact</a></li>
+                </ul>
+            </div>
+            <div class="w-[20%] mt-28">
+                <h1 class="text-2xl font-bold mb-5">Address</h1>
+                <p>Jl. Al-Muwahhiddin, Karangtengah, Cibadak, Kab. Sukabumi.
+                    Sukabumi 43351</p>
+            </div>
+            <div class="w-[30%] mt-28">
+                <h1 class="text-2xl font-bold mb-5">Contact</h1>
+                <ul>
+                    <li><a href="#"><img src="./property/ant-design_phone-filled.png" alt="">(0266) 532510</a></li>
+                    <li><a href="#"><img src="./property/clarity_email-solid.png" alt="">     smkn1_cibadak@yahoo.co.id</a></li>
+                </ul>
+            </div>
+        </div>
+    </footer>
 </body>
